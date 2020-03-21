e6115fbeaf2fcc08f8f46f890ad0f0127ce306ea
bibelstudy <george.zirconium@gmail.com>
2020-03-22 00:40:51 +0100

    initialisation

---
+++ code.js
@@ -0,0 +1,10 @@
+<script type="text/javascript">
+  (function(){
+      var ga = document.createElement('script');
+      ga.src = 'https://www.bibleserver.com/api/parser.js?key=be4b4dd5942abc87f9bfae618739504735979c2d&lang=de';
+      ga.setAttribute('async', 'true');
+      document.documentElement.firstChild.appendChild(ga);
+  })();
+  var bsQuery = '.level1'; // jQuery-Selektor für Wrapper der Suche (optional)
+  var bsTrl   = 'SLT';      // Verlinkte Übersetzung (optional)
+</script>
